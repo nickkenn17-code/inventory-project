49cb7c5aae5fc7e8794a40b80112ff7fd5d05e5d
Nicholas Kenny <nickkenn17@gmail.com>
2026-05-01 13:55:09 +0700

    Completed Phase 2: Database Provisioning with Docker Compose

--- database.py
+++ database.py
@@ -1,16 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# We will connect to a PostgreSQL container named 'postgres_db' later.
-# Format: postgresql://username:password@host:port/database_name
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:password@localhost:5432/inventory_db"
+# Fetch the URL from the environment (used in Docker), or default to localhost (for local testing)
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "postgresql://admin:password@localhost:5432/inventory_db"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependency to get a database session for our API routes
 def get_db():
     db = SessionLocal()
     try:
